30e7b4460078d334f61d9a9d942f6a524f15707b
Nurcholis Koem <nurcholis.a.koem@gmail.com>
2024-03-08 17:45:31 +0700

    update login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -95,7 +95,7 @@
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
-
+                            <img src="http://34.142.197.204:8080/storage/verifikasi/1709886927.png">
                                 <h1 class="display-4 font-weight-bold mb-2">Good Morning</h1>
                                 <h5 class="font-weight-normal text-muted-transparent">Gorontalo, Indonesia</h5>
                             </div>
