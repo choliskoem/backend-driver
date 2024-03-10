fff71a34cc39396ebf8d4a708fd2ccaff4e1e53d
Nurcholis Koem <nurcholis.a.koem@gmail.com>
2024-03-11 01:26:23 +0700

    update index user

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -12,9 +12,7 @@
         <section class="section">
             <div class="section-header">
                 <h1>Users</h1>
-                <div class="section-header-button">
-                    <a href="{{ route('user.create') }}" class="btn btn-primary">Add New</a>
-                </div>
+
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Users</a></div>
@@ -80,11 +78,11 @@
                                                 <td>
 
                                                     <div class="d-flex justify-content-center">
-                                                        <a href="{{ route('user.edit', $user->id) }}"
+                                                        {{-- <a href="{{ route('user.edit', $user->id) }}"
                                                             class="btn btn-sm btn-info btn-icon">
                                                             <i class="fas fa-edit"></i>
                                                             Edit
-                                                        </a>
+                                                        </a> --}}
                                                         <form action="{{ route('user.destroy', $user->id) }}" method="POST"
                                                             class="ml-2">
                                                             <input type="hidden" name="_method" value="DELETE">
