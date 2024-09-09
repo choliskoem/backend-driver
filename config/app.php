86575abeace61dfed760d8d3763bb11f2b472d25
Nurcholis Koem <nurcholis.a.koem@gmail.com>
2024-09-09 20:57:18 +0700

    update

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://dd.insitama.co.id/'),
+    'url' => env('APP_URL', 'https://dd.insitama.co.id'),
 
     'asset_url' => env('ASSET_URL'),
 
